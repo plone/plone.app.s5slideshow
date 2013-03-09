b24758c50b4d0c6612835ce31adf68cef40a627d
David Glick <david@glicksoftware.com>
2013-03-09 10:56:04 -0800

    prep for release

--- README.rst
+++ README.rst
@@ -1,9 +1,10 @@
 Introduction
 ============
 
-This addon integrate s5 to Plone.
+This addon integrates the s5 slideshow to Plone.
 
-Version: 1.1
+It is intended for use with Plone 4.4. Previous versions of Plone
+included the s5 slideshow feature in core.
 
 About S5
 ========
@@ -16,3 +17,12 @@ XHTML can look at the markup and figure out how to adapt it to their particular
 needs. Anyone familiar with CSS can create their own slide show theme. It's 
 totally simple, and it's totally standards-driven.
 
+Installation and Usage
+======================
+
+Go to Site Setup and activate the 'S5 Slideshow support' add-on.
+
+Now when you edit a document and go to the Settings tab there is a
+checkbox to enable presentation mode. When it is enabled a link
+will be shown at the top of the document to access the presentation
+mode.
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1.0'
+version = '1.0'
 
 setup(name='plone.app.s5slideshow',
       version=version,
       description="S5 Slideshow plone integration",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=(open("README.rst").read() + "\n" +
+                        open(os.path.join("docs", "HISTORY.txt")).read()),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        ],
+          "Programming Language :: Python",
+      ],
       keywords='',
       author='',
       author_email='',
@@ -23,12 +23,20 @@ setup(name='plone.app.s5slideshow',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
+          'archetypes.schemaextender',
+          'plone.app.layout',
+          'plone.memoize',
+          'Products.Archetypes',
           'Products.ATContentTypes',
-          'Products.CMFPlone',
+          'Products.CMFCore',
+          'Products.CMFPlone>=4.4a1.dev0',
+          'setuptools',
+          'Zope2',
+          'zope.interface',
+          'zope.i18n',
           # -*- Extra requirements: -*-
       ],
-      extras_require = {'test': ['plone.app.testing',]},
+      extras_require={'test': ['plone.app.testing']},
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
